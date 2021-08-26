c93414a9b4e83ad78da26fa4405e56cfd44b3d4d
Caio Tony <caiotony16@gmail.com>
2021-08-26 15:09:23 +0100

    feat: ensure that the controllers returns a 400 error if a required field is missing

--- src/savePerson/save_person.ts
+++ src/savePerson/save_person.ts
@@ -3,8 +3,15 @@ import * as R from 'ramda';
 export class savePerson implements usecase{
   handle(person_data: u_person): response {
 
+    const required_fields = ['name', 'age', 'password'];
     if (R.isEmpty(person_data)) {
       return g_response<Error>(400, new Error('incomming object is empty'))
+    }
+
+    for (const iterator of required_fields) {
+      if (!person_data[iterator]) {
+        return g_response<Error>(400, new Error('there are missing fields'))
+      }
     }
 
     return g_response<s_person>(200, {
