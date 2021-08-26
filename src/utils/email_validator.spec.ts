4df7fe754f764e563c89df8adfa4d3cf1c6fd511
Caio Tony <caiotony16@gmail.com>
2021-08-26 18:48:21 +0100

    test: ensure validate method calls isEmail from validator with proper email

--- src/utils/email_validator.spec.ts
+++ src/utils/email_validator.spec.ts
@@ -5,10 +5,12 @@ jest.mock('validator', () => ({
 }))
 const make_sut = () => new Email_validator()
 describe('email validator adaptor', () => {
+  afterEach(() => {
+    jest.mock('validator').clearAllMocks()
+  })
   test('ensure validate returns true if email is valid', () => {
     const sut = make_sut();
     const email = 'valid.email@gmail.com';
-
     const response = sut.validate(email);
     expect(response).toBe(true)
   });
@@ -17,9 +19,17 @@ describe('email validator adaptor', () => {
     const sut = make_sut();
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const email = 'valid.email@gmail.com';
-
     const response = sut.validate(email);
     expect(response).toBe(false)
+  })
+  
+  test('ensure validate method calls isEmail from validator with proper email', () => {
+    const sut = make_sut();
+    const email = 'pumpum@onodera.com'
+    const validator_spy = jest.spyOn(validator, 'isEmail');
+    sut.validate(email);
+    expect(validator_spy).toHaveBeenCalledTimes(1);
+    expect(validator_spy).toHaveBeenCalledWith(email)
   })
   
 })
