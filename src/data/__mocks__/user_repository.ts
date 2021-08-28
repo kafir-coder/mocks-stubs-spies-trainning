23426747f03a7298b244751a2c5a1a2c74113301
Caio Tony <caiotony16@gmail.com>
2021-08-28 10:49:39 +0100

    test: ensures user_repository.save is called from data layer

--- src/data/__mocks__/user_repository.ts
+++ src/data/__mocks__/user_repository.ts
@@ -1,4 +1,9 @@
+import { s_person, u_person } from "../../savePerson/protocols";
 import { persist_user_repository } from "../protocols";
 
-class user_repository implements persist_user_repository {
+export class user_repository implements persist_user_repository {
+  async save(person_data: s_person): Promise<s_person> {
+    const {id, age, name, password} = person_data;
+    return new Promise(resolve => resolve({id, name, age, password}))
+  }
 }
--- src/data/fs_persists_user.spec.ts
+++ src/data/fs_persists_user.spec.ts
@@ -1,4 +1,4 @@
-import { u_person } from '../savePerson/protocols';
+import { s_person, u_person } from '../savePerson/protocols';
 import { fs_persists_user } from './fs_persists_user';
 import { user_repository } from './__mocks__/user_repository'
 describe('fs persists user data layer', () => {
@@ -16,5 +16,23 @@ describe('fs persists user data layer', () => {
     const person_data: u_person = {}
     const result = sut.add(person_data)
     expect(result).rejects.toThrow()
+  });
+
+  test('ensures user_repository.save is called from data layer', async () => {
+    const {sut, user_repository_stub} = make_sut();
+    const person_data: u_person = {
+      name: 'Tony Jelinek von Kyanda',
+      age: 19,
+      password: '^IloveAiko$',
+      email: 'mogli@keit.com'
+    };
+
+    const user_repository_spy = jest.spyOn(user_repository_stub, 'save');
+    sut.add(person_data)
+    expect(user_repository_spy).toBeCalledTimes(1)
+    expect(user_repository_spy).toHaveBeenCalledWith({
+      id: 'mogli@keit.com19',
+      ...person_data
+    })
   });
 })
