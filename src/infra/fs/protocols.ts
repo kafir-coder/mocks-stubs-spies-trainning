90e2a3d2ddb322f12a3e47734dabf3be96a13532
Caio Tony <caiotony16@gmail.com>
2021-08-28 14:16:10 +0100

    feat: ensure user_repository.loadById calls fsPromises.readFile

--- src/infra/fs/protocols.ts
+++ src/infra/fs/protocols.ts
@@ -3,3 +3,7 @@ import { s_person } from "../../savePerson/protocols";
 export interface fs_persists_user_repository {
   save(person_data: s_person): Promise<void>
 }
+
+export interface fs_load_user_repository {
+  loadById(id: string): Promise<s_person>
+}
