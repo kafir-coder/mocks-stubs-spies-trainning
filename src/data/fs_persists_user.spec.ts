ae0d0ae601ccda37a4b31eaf233ca8426087a834
Caio Tony <caiotony16@gmail.com>
2021-08-28 10:18:46 +0100

    refactor: inject user_repository into fs_persisits_user

--- src/data/fs_persists_user.spec.ts
+++ src/data/fs_persists_user.spec.ts
@@ -1,16 +1,17 @@
 import { u_person } from '../savePerson/protocols';
 import { fs_persists_user } from './fs_persists_user';
+import { user_repository } from './__mocks__/user_repository'
 describe('fs persists user data layer', () => {
   const make_sut = () => {
-    const user_repository = new user_repository()
-    const fs_persists = new fs_persists_user(user_repository);
+    const user_repository_stub = new user_repository()
+    const fs_persists = new fs_persists_user(user_repository_stub);
     return {
-      user_repository,
-      fs_persists
+      user_repository_stub,
+      sut: fs_persists
     };
   }
   test('ensures person_data isn\'t empty', async () => {
-    const sut = make_sut();
+    const {sut} = make_sut();
     //@ts-ignore
     const person_data: u_person = {}
     const result = sut.add(person_data)
--- src/data/fs_persists_user.ts
+++ src/data/fs_persists_user.ts
@@ -1,8 +1,11 @@
 import { persists_user } from "../domain/persists_user";
 import { s_person, u_person } from "../savePerson/protocols";
+import { user_repository } from '../infra/fs/user_repository'
 import * as R from 'ramda';
 export class fs_persists_user implements persists_user {
-  constructor() {}
+  constructor(
+    fs_persists_user_repository: user_repository
+  ) {}
   async add(person_data: u_person): Promise<s_person> {
     if (R.isEmpty(person_data)) {
       return new Promise((_resolve, reject) => reject('person data is empty'))
